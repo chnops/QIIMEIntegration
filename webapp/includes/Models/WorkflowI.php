7f48fa4b33e999bc972f05191d06d2917300fd1f
Aaron Sharp <sharp.aaron.r@gmail.com>
2014-05-14 13:23:56 -0500

    To Models\Workflow: Made tests and simplified

--- webapp/includes/Models/WorkflowI.php
+++ webapp/includes/Models/WorkflowI.php
@@ -3,12 +3,14 @@
 namespace Models;
 
 interface WorkflowI {
-	public function __construct(OperatingSystemI $operatingSystem);
+	public function __construct(\Database\DatabaseI $database, OperatingSystemI $operatingSystem);
 	public function getSteps();
 	public function getNextStep($step);
 	public function getPreviousStep($step);
 	public function getCurrentStep($controller);
-	public function getController($step, \Database\DatabaseI $database);
+	public function getController($step);
 
-	public function getDefaultProject(\Database\DatabaseI $database);
+	public function getNewProject();
+	public function findProject($username, $projectId);
+	public function getAllProjects($username);
 }
