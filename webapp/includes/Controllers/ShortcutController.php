d2a476f3b063d2f9e76ffb4d84264aaf6635a146
Aaron Sharp <sharp.aaron.r@gmail.com>
2014-05-15 17:05:51 -0500

    Small state-persistance improvement in Controller

--- webapp/includes/Controllers/ShortcutController.php
+++ webapp/includes/Controllers/ShortcutController.php
@@ -7,20 +7,18 @@ class ShortcutController extends Controller {
 	protected $subTitle = "Full Analysis";
 
 	public function parseSession() {
-		return;
+		$this->project = $this->workflow->getNewProject();
 	}
 
 	public function parseInput() {
 		if (!empty($_POST)) {
 			$this->hasResult = true;
-			$project = $this->workflow->getNewProject();
-			$this->result = $project->processInput($_POST);
+			$this->result = $this->project->processInput($_POST);
 		}
 	}
 
 	public function getInstructions() {
-		$project = $this->workflow->getNewProject();
-		$this->help = $project->renderHelp();
+		$this->help = $this->project->renderHelp();
 
 		$javascript = "<script type=\"text/javascript\">document.getElementById('navigation').style.display=\"none\"</script>";
 		$css = "<style>input[type=\"text\"],input[type=\"file\"],select{display:block}</style>";
@@ -29,8 +27,7 @@ class ShortcutController extends Controller {
 	}
 
 	public function getForm() {
-		$project = $this->workflow->getNewProject();
-		return $project->renderForm();
+		return $this->project->renderForm();
 	}
 	public function getSessionData() {
 		return "No login required";
