d989870075e8e6fc9887a9d2fdce8fb49f8aa68b
Aaron Sharp <sharp.aaron.r@gmail.com>
2014-06-20 15:37:47 -0500

    Download: large files work, better html escaping

--- webapp/download.php
+++ webapp/download.php
@@ -8,7 +8,7 @@ if (!isset($_SESSION['username']) || !isset($_SESSION['project_id'])) {
 	echo "<p>You must <a href=\"index.php\">login</a> and select a project</p>";
 	exit;
 }
-if ((!isset($_GET['run']) && !isset($_GET['uploaded'])) || !isset($_GET['file_name'])) {
+if (!isset($_GET['run']) || !isset($_GET['file_name'])) {
 	header('HTTP/1.1 400 Bad request');
 	echo "<p>You must provide a run id and file name to download (url)</p>";
 	exit;
@@ -16,27 +16,36 @@ if ((!isset($_GET['run']) && !isset($_GET['uploaded'])) || !isset($_GET['file_na
 $operatingSystem = new \Models\MacOperatingSystem();
 $database = new \Database\PDODatabase($operatingSystem);
 $actualPath = "./projects/u" . $database->getUserRoot($_SESSION['username']) . "/p" . $_SESSION['project_id'];
-if (isset($_GET['uploaded']) && $_GET['uploaded']) {
+if ($_GET['run'] == -1) {
 	$actualPath .= "/uploads/" . $_GET['file_name'];
 }
 else {
 	$actualPath .= "/r" . $_GET['run'] . "/" . $_GET['file_name'];
 }
 
-if ($_GET['as_text']) {
-	$maxLen = 2000;
-	$contents = file_get_contents($actualPath, $useIncludePath = false, $context = NULL, $offset = -1, $maxLen);
-	if (defined('ENT_SUBSTITUE')) {
-		echo htmlentities($contents, ENT_COMPAT | ENT_HTML5 | ENT_SUBSTITUTE);
+if (isset($_GET['as_text']) && $_GET['as_text']) {
+	try {
+		$maxLen = 2000;
+		$contents = file_get_contents($actualPath, $useIncludePath = false, $context = NULL, $offset = -1, $maxLen);
+		if(strlen($contents) == 0) {
+			echo "<div style=\"font-family:'Comic Sans MS', cursive, sans-serif\">	File is empty	</div>";
+		}
+		else {
+			$helper = \Utils\Helper::getHelper();
+			echo $helper->htmlentities($contents);
+
+			if (strlen($contents) == $maxLen) {
+				echo "...";
+			}
+		}
 	}
-	else {
-		echo htmlentities($contents, ENT_IGNORE);
-	}
-	if (strlen($contents) == $maxLen) {
-		echo "...";
+	catch (Exception $ex) {
+		error_log($ex->getMessage());
+		echo "<div style=\"font-family: 'Comic Sans MS', cursive, sans-serif\">	Error accessing file: Please see the error log or system administrator</div>";
 	}
 }
 else {
+	ob_end_clean();
 	// TODO reference database for file specific information (e.g. text type, size)
 	header('Content-Type: application/octet-stream');
 	header("Content-Transfer-Encoding: Binary"); 
--- webapp/includes/Utils/Helper.php
+++ webapp/includes/Utils/Helper.php
@@ -21,4 +21,24 @@ class Helper {
 		}
 		return $formattedArray;
 	}
+
+	public function htmlentities($string) {
+		if ($string === 0) {
+			return 0;
+		}
+		else if (!$string) {
+			return "";
+		}
+
+		if (defined('ENT_SUBSTITUE')) {
+			$stringEsc = htmlentities($string, ENT_QUOTES | ENT_HTML5 | ENT_SUBSTITUTE);
+		}
+		else {
+			$stringEsc = htmlentities($string, ENT_QUOTES | ENT_IGNORE);
+		}
+		if (!$stringEsc) {
+			throw new \Exception("Problem with html special char escaping; dropped whole string");
+		}
+		return $stringEsc;
+	}
 }
