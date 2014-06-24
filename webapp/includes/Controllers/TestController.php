9c22dd9850c2f235c38211faf6d86f09d150c112
Aaron Sharp <sharp.aaron.r@gmail.com>
2014-06-24 16:33:30 -0500

    Improved past run display
    DO NOT DEPLOY: This change isn't independently functional

--- webapp/includes/Controllers/TestController.php
+++ webapp/includes/Controllers/TestController.php
@@ -4,7 +4,7 @@ namespace Controllers;
 
 class TestController extends Controller {
 	public function parseSession() {
-		return;
+		parent::parseSession();
 	}
 	public function parseInput() {
 		if (isset($_GET['clean'])) {
@@ -18,12 +18,32 @@ class TestController extends Controller {
 	public function renderInstructions() {
 		ob_start();
 
-		echo "<p>Testing htmlentities</p>";
+		echo "<p>Testing past run display</p>";
 
-		echo "if false: " . htmlentities(false) . "<br/>";
-		echo "if 0: " . htmlentities(0) . "<br/>";
-		echo "if empty: " . htmlentities("") . "<br/>";
+		$helper = \Utils\Helper::getHelper();
+		$pastRuns = $this->project->getPastScriptRuns();
+//		$pastRuns = $helper->categorizeArray($pastRuns, "name");
 
+		echo "<style>
+			.accordion h4,.accordion div{margin-bottom:0em;margin-top:0em;padding:.25em}
+			</style>";
+
+		echo "<div class=\"accordion\" style=\"display:inline-block\">";
+		foreach ($pastRuns as $run) {
+			echo "<h4 onclick=\"hideMe($(this).next())\">Run id: {$run['id']}</h4>";
+			echo "<div><strong>User input:</strong> {$run['input']}";
+			if (!empty($run['file_names'])) {
+				echo "<br/><strong>Generated files</strong><ul>";
+				foreach($run['file_names'] as $file) {
+					echo "<li>{$file}</li>";
+				}
+				echo "</ul>";
+			}
+			echo "</div>";
+		}
+		echo "</div>";
+
+		echo "<script type=\"text/javascript\">$(function() { $('.accordion').width($('.accordion').width()) })</script>";
 		return ob_get_clean();
 	}
 	public function renderForm() {
