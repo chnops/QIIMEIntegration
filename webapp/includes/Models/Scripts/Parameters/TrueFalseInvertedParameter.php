c613c2f71effc9d2f0093596aab0e5796b147651
FY <snisiarc@gmail.com>
2014-07-22 13:35:14 -0500

    More intuitive true false parameter relationship

--- webapp/includes/Models/Scripts/Parameters/TrueFalseInvertedParameter.php
+++ webapp/includes/Models/Scripts/Parameters/TrueFalseInvertedParameter.php
@@ -2,7 +2,7 @@
 
 namespace Models\Scripts\Parameters;
 
-class TrueFalseInvertedParameter extends DefaultParameter {
+class TrueFalseInvertedParameter extends TrueFalseParameter {
 	public function __construct($name) {
 		$this->name = $name;
 		$this->value = true;
@@ -12,21 +12,5 @@ class TrueFalseInvertedParameter extends DefaultParameter {
 			return $this->name;
 		}
 		return "";
-	}
-	public function renderForForm($disabled, \Models\Scripts\ScriptI $script) {
-		$disabledString = ($disabled) ? " disabled" : "";
-		$checked = ($this->value) ? " checked" : "";
-		return "<label for=\"{$this->name}\"><input type=\"checkbox\" name=\"{$this->name}\"{$checked}{$disabledString}/> {$this->name}
-		   <a class=\"param_help\" id=\"{$this->getJsVar($script->getJsVar())}\">&amp;</a></label>";
-	}
-
-	public function acceptInput(array $input) {
-		parent::acceptInput($input);
-		if (!isset($input[$this->name])) {
-			$this->setValue(false);
-		}
-		else {
-			$this->setValue(true);
-		}
 	}
 }
